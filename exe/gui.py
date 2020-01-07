e8cf0135e62fad4319b5b458402b8ec464de1181
feimaomiao <lcpmatthew@gmail.com>
2020-01-06 18:42:12 -0800

    updated user interface. Still in building progress

--- exe/gui.py
+++ exe/gui.py
@@ -4,8 +4,10 @@ from solver import *
 
 def initialise(root):
 	root.title('sudoku')
+	root.resizable(False, False)
 	root.minsize(453,435)
 	root.update_idletasks()
+	root.overrideredirect(True)
 	width = root.winfo_width()
 	height = root.winfo_height()
 	x = (root.winfo_screenwidth() // 2) - (width // 2)
@@ -16,40 +18,51 @@ class board(object):
 	def __init__(self, master):
 		self.master= master
 		self.numList = [[0 for c in range(9)] for i in range(9)]
-		self.frame = Frame(self.master)
 		
 	def draw_board(self):
 		render = ImageTk.PhotoImage(Image.open('sudoku.png'))
 		self.img = Label(self.master, image=render)
 		self.img.image=render
 		self.img.pack()
-		self.img.focus_set()
-		self.img.bind('<Key>', self.mouseClick)
-		self.img.bind('<Button-1>',self.mouseClick)
+		# self.img.bind('<Key>', self.mouseClick)
+		# self.img.bind('<Button-1>',self.mouseClick)
 
 
 	def mouseClick(self, event):
-		print(event)
-		# self.frame.pack()
-		p = Label(self.master, text='1', fg='blue',font=(None, 25))
-		p.place(x=410,y=14)
+		self.clicker.destroy()
+		self.master.update_idletasks()
+		self.master.overrideredirect(False)
+		self.master.bind('<Key>', quit)
+		loc_x =event.x
+		loc_y = event.y
+		yposition = (loc_y/453) *9
+		xposition = (loc_x/435) *9
+		print(xposition, yposition)
+		self.layer_of_text()
 
 	def layer_of_text(self):
-		self.frame.pack()
 		xcoordinate = {0: 17,1: 65,2: 114,3: 165,4: 215,5: 263,6: 314,7: 362,8: 410}
 		ycoordinate = {0: 14,1: 59,2: 105,3: 150,4: 196,5: 247,6: 295,7: 341,8: 387}
 		lst= []
-		# self.frame.focus_set()
+		
 		for count, i in enumerate(self.numList):
 			lst.append([])
 			for j in i:
 				# text = StringVar(self.master, j)
 				lst[count].append(Label(self.master, text=j, fg='blue',font=(None,25)))
-		print(lst)
 		for i in range(9):
 			for j in range(9):
 				lst[i][j].place(x=xcoordinate.get(j), y=ycoordinate.get(i))
-		self.frame.pack()
+		# self.img.bind('<Button-1>',self.mouseClick)
+		self.clicker = Tk()
+		self.clicker.attributes('-alpha',0.0)
+		initialise(self.clicker)
+		self.clicker.focus_set()
+		self.clicker.bind('<Button-1>', self.mouseClick)
+		
+
+
+
 
 
 # A.y == 14
@@ -82,6 +95,7 @@ if __name__ == '__main__':
 	root = Tk()
 	initialise(root)
 	sudokuB = board(root)
+	# root.bind('<Button-1>', sudokuB.mouseClick)
 	sudokuB.draw_board()
 	sudokuB.layer_of_text()
 	root.mainloop()
